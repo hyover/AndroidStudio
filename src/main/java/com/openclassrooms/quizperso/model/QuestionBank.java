d648952e70ddf3d64bd0112ba74f29f04e43cbde
Valentin FONTAINE <89849717+hyover@users.noreply.github.com>
2021-09-06 18:21:55 +0200

    Add files via upload

---
+++ src/main/java/com/openclassrooms/quizperso/model/QuestionBank.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.quizperso.model;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class QuestionBank implements Serializable { // Tableau particulier avec Serializable
+
+    private List<Question> mQuestionList;
+    private int mNextQuestionIndex;
+
+    public QuestionBank(List<Question> questionList) {
+        mQuestionList = questionList;
+
+        Collections.shuffle(mQuestionList); // mélange alétoirement les questions
+    }
+    public Question getCurrentQuestion() {
+        return mQuestionList.get(mNextQuestionIndex); // Choisir question
+    }
+
+    public Question getNextQuestion() { // Question suivante
+        mNextQuestionIndex++;
+        return getCurrentQuestion();
+    }
+}
